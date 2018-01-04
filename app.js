2d3afcb90474250c24dcbd45ae4959439519e07b
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2018-01-04 16:25:26 -0700

    file

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
 var inquirer = require("inquirer");
 
-console.log("Welcome to Command Line Hangman!");
+var init = function() {
+    console.log("Welcome to Command Line Hangman!");
 
-inquirer.prompt([
-    {
-        type: "confirm",
-        name: "confirm",
-        message: "Would you like to play?"
-    }
-]).then(function(res) {
-    if (res.confirm) {
-        console.log("Let's Play!");
-        game();
-    } else {
-        console.log("Thanks for playing!");
-    }
-});
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "confirm",
+            message: "Would you like to play?"
+        }
+    ]).then(function(res) {
+        if (res.confirm) {
+            console.log("Let's Play!");
+            game();
+        } else {
+            console.log("Thanks for playing!");
+        }
+    });
+};
 
+var reset = function() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "confirm",
+            message: "Would you like to play again?"
+        }
+    ]).then(function(res) {
+        if (res.confirm) {
+            console.log("Let's Play Again!");
+            game();
+        } else {
+            console.log("Thanks for playing!");
+        }
+    });
+};
 
 var game = function() {
     console.log("Game Start!");
 
     var word = require("./word.js");
 
+    //render word w/blanks
+
+    //prompt for user guess
+    
+    reset();
 };
+
+init();
+
